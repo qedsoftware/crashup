12f94e9dd9b5e589bfc1e0fadd599337b0a326c5
Jakub Sroka <jakub1@qed.ai>
2016-04-08 21:24:45 +0200

    added script that runs clang-format
    Change-Id: If7a7d74dfc2366130191d724534b36237f3d2a1f

---
+++ format.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+def do_call(*args):
+  print("[{}]> {}".format(os.getcwd(), ' '.join(args)))
+  subprocess.check_call(args)
+
+cmd = (
+        'shopt -s globstar && clang-format-3.7 -style=LLVM -i '
+        'crashup/**/*.cpp crashup/**/*.hpp demoapp/**/*.cpp demoapp/**/*.hpp '
+)
+
+print "FORMATTING ALL FILES USING CLANG-FORMAT"
+do_call("bash", "-c", cmd)
